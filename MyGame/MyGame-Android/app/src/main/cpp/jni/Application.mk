5518b18982d4bd8329fc1f7cbdef631e83760804
Holoweird <hoainam990@gmail.com>
2015-03-02 22:17:35 +0700

    nam

--- MyGame/MyGame-Android/app/src/main/cpp/jni/Application.mk
+++ MyGame/MyGame-Android/app/src/main/cpp/jni/Application.mk
@@ -4,8 +4,8 @@ NDK_TOOLCHAIN_VERSION := 4.9
 
 APP_ABI := armeabi-v7a
 
-#  Enable C++11. However, pthread, rtti and exceptions aren’t enabled 
-APP_CPPFLAGS += -std=c++11
+#  Enable C++11. However, pthread, rtti and exceptions aren’t enabled
+APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -Wno-deprecated-declarations -Wno-multichar -Wno-literal-suffix
 
 # Instruct to use the static GNU STL implementation
 APP_STL := gnustl_static
---
+++ MyGame/MyGame-Android/app/src/main/cpp/jni/_Android.mk
@@ -0,0 +1,98 @@
+
+LOCAL_PATH			     := $(call my-dir)
+
+SDK_PATH                 := ../../../../../../../sdk
+
+SDK_EXTERNAL_PATH        := $(SDK_PATH)/External
+
+SDK_FRM_INC_PATH		 := $(SDK_PATH)/Framework/Inc
+
+SDK_FRM_PATH			 := $(SDK_PATH)/Framework/Framework
+SDK_FRM_ANDROID_PATH	 := $(SDK_FRM_PATH)/Android
+SDK_FRM_MODEL_PATH	     := $(SDK_FRM_PATH)/Model
+SDK_FRM_OPENGL_PATH		 := $(SDK_FRM_PATH)/OpenGLES
+
+SDK_MY_FRM_PATH          := $(SDK_PATH)/MyFramework
+SDK_MY_FRM_ANDROID_PATH  := $(SDK_MY_FRM_PATH)/Android
+SDK_MY_FRM_OPENGL_PATH   := $(SDK_MY_FRM_PATH)/OpenGLES
+
+GAME_PATH                := ../../../../../../
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := MyGame
+
+LOCAL_SRC_FILES := $(SDK_FRM_PATH)/FrmApplication.cpp \
+				   $(SDK_FRM_PATH)/FrmInput.cpp \
+				   $(SDK_FRM_PATH)/FrmPackedResource.cpp \
+				   $(SDK_FRM_PATH)/FrmUtils.cpp \
+				   $(SDK_FRM_ANDROID_PATH)/FrmApplication_Platform.cpp \
+				   $(SDK_FRM_ANDROID_PATH)/FrmFile_Platform.cpp \
+				   $(SDK_FRM_ANDROID_PATH)/FrmNoise_Platform.cpp \
+				   $(SDK_FRM_ANDROID_PATH)/FrmStdlib_Platform.cpp \
+				   $(SDK_FRM_ANDROID_PATH)/FrmUtils_Platform.cpp \
+				   $(SDK_FRM_MODEL_PATH)/FrmAnimation.cpp \
+				   $(SDK_FRM_MODEL_PATH)/FrmAnimationTrack.cpp \
+				   $(SDK_FRM_MODEL_PATH)/FrmIndexBuffer.cpp \
+				   $(SDK_FRM_MODEL_PATH)/FrmJoint.cpp \
+				   $(SDK_FRM_MODEL_PATH)/FrmMaterial.cpp \
+				   $(SDK_FRM_MODEL_PATH)/FrmMemoryBuffer.cpp \
+				   $(SDK_FRM_MODEL_PATH)/FrmMeshSurfaceArray.cpp \
+				   $(SDK_FRM_MODEL_PATH)/FrmModel.cpp \
+				   $(SDK_FRM_MODEL_PATH)/FrmModelIO.cpp \
+				   $(SDK_FRM_MODEL_PATH)/FrmModelMesh.cpp \
+				   $(SDK_FRM_MODEL_PATH)/FrmNamedId.cpp \
+				   $(SDK_FRM_MODEL_PATH)/FrmVertexBuffer.cpp \
+				   $(SDK_FRM_MODEL_PATH)/FrmVertexFormat.cpp \
+				   $(SDK_FRM_MODEL_PATH)/FrmVertexProperty.cpp \
+				   $(SDK_FRM_OPENGL_PATH)/FrmMesh.cpp \
+				   $(SDK_FRM_OPENGL_PATH)/FrmPackedResourceGLES.cpp \
+				   $(SDK_FRM_OPENGL_PATH)/FrmResourceGLES.cpp \
+				   $(SDK_FRM_OPENGL_PATH)/FrmShader.cpp \
+				   $(SDK_FRM_OPENGL_PATH)/FrmUtilsGLES.cpp \
+				   $(SDK_MY_FRM_PATH)/MyCamera.cpp \
+				   $(SDK_MY_FRM_PATH)/MyCamera2D.cpp \
+				   $(SDK_MY_FRM_PATH)/MyInput.cpp \
+				   $(SDK_MY_FRM_PATH)/MyMath.cpp \
+				   $(SDK_MY_FRM_PATH)/MyOrthoCamera.cpp \
+				   $(SDK_MY_FRM_PATH)/MyPerspectiveCamera.cpp \
+				   $(SDK_MY_FRM_PATH)/MyScreen.cpp \
+				   $(SDK_MY_FRM_PATH)/MyScreenManager.cpp \
+				   $(SDK_MY_FRM_PATH)/MyTimer.cpp \
+				   $(SDK_MY_FRM_PATH)/MyUIButton.cpp \
+				   $(SDK_MY_FRM_PATH)/MyUIWidget.cpp \
+				   $(SDK_MY_FRM_PATH)/MyUtils.cpp \
+				   $(SDK_MY_FRM_ANDROID_PATH)/MyUtils_Platform.cpp \
+				   $(SDK_MY_FRM_OPENGL_PATH)/MyBasicMesh.cpp \
+				   $(SDK_MY_FRM_OPENGL_PATH)/MyFileMesh1.cpp \
+				   $(SDK_MY_FRM_OPENGL_PATH)/MyInputLayout.cpp \
+				   $(SDK_MY_FRM_OPENGL_PATH)/MyMesh.cpp \
+				   $(SDK_MY_FRM_OPENGL_PATH)/MyShader.cpp \
+				   $(SDK_MY_FRM_OPENGL_PATH)/MySkinnedMesh1.cpp \
+				   $(SDK_MY_FRM_OPENGL_PATH)/MySkinnedMesh2.cpp \
+				   $(SDK_MY_FRM_OPENGL_PATH)/MySpriteBatch.cpp \
+				   $(SDK_MY_FRM_OPENGL_PATH)/MyTexture.cpp \
+				   $(SDK_MY_FRM_OPENGL_PATH)/MyVertex.cpp \
+				   $(GAME_PATH)/MainGame.cpp \
+				   $(GAME_PATH)/MenuScreen.cpp \
+				   $(GAME_PATH)/PlayScreen.cpp
+				   
+LOCAL_C_INCLUDES	:= $(LOCAL_PATH)/$(SDK_EXTERNAL_PATH) \
+					   $(LOCAL_PATH)/$(SDK_FRM_INC_PATH) \
+					   $(LOCAL_PATH)/$(SDK_FRM_PATH)	\
+					   $(LOCAL_PATH)/$(SDK_FRM_ANDROID_PATH) \
+					   $(LOCAL_PATH)/$(SDK_FRM_MODEL_PATH) \
+					   $(LOCAL_PATH)/$(SDK_FRM_OPENGL_PATH) \
+					   $(LOCAL_PATH)/$(SDK_MY_FRM_PATH)	\
+					   $(LOCAL_PATH)/$(SDK_MY_FRM_ANDROID_PATH) \
+					   $(LOCAL_PATH)/$(SDK_MY_FRM_OPENGL_PATH)
+				   
+LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv2 -lOpenSLES
+
+LOCAL_CFLAGS	+= -Wno-write-strings -Wno-conversion-null
+
+LOCAL_STATIC_LIBRARIES := android_native_app_glue
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android/native_app_glue)

6bf656ff1ab2b37274ae392589471cf202293305
hoainam1593 <hoainam1593@yahoo.com>
2015-05-25 15:25:21 -0700

    nam

--- MyGame/Misc/HeroSkill.h
+++ MyGame/Misc/HeroSkill.h
@@ -50,3 +50,54 @@ public:
 private:
 
 };
+
+class HeroSkill_Bladefall : public HeroSkill
+{
+public:
+	HeroSkill_Bladefall(
+		const MyString& _name,
+		int _damage,
+		int _cost,
+		float _coolDownTime,
+		Texture* _avatar)
+		: HeroSkill(_name, _damage, _cost, _coolDownTime, _avatar)
+	{}
+	~HeroSkill_Bladefall(){}
+
+private:
+
+};
+
+class HeroSkill_DecimationDay : public HeroSkill
+{
+public:
+	HeroSkill_DecimationDay(
+		const MyString& _name,
+		int _damage,
+		int _cost,
+		float _coolDownTime,
+		Texture* _avatar)
+		: HeroSkill(_name, _damage, _cost, _coolDownTime, _avatar)
+	{}
+	~HeroSkill_DecimationDay(){}
+
+private:
+
+};
+
+class HeroSkill_JustDesserts : public HeroSkill
+{
+public:
+	HeroSkill_JustDesserts(
+		const MyString& _name,
+		int _damage,
+		int _cost,
+		float _coolDownTime,
+		Texture* _avatar)
+		: HeroSkill(_name, _damage, _cost, _coolDownTime, _avatar)
+	{}
+	~HeroSkill_JustDesserts(){}
+
+private:
+
+};

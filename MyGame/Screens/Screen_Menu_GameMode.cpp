c00d3eabfdf3dbbf734ae1e1d48dd2ccccff9ff2
hoainam1593 <hoainam1593@yahoo.com>
2015-06-06 11:11:40 +0700

    nam

--- MyGame/Screens/Screen_Menu_GameMode.cpp
+++ MyGame/Screens/Screen_Menu_GameMode.cpp
@@ -5,6 +5,22 @@
 #include "Screen_Menu_GameMode.h"
 #include <MyScreenManager.h>
 
+//=================================================================================================================================
+//
+// Constants
+//
+//=================================================================================================================================
+
+static const float BTN_HDIST = 10.0f;
+static const float SCREEN_TITLE_MARGIN = 15.0f;
+static const float BTN_BACK_MARGIN = 10.0f;
+
+//=================================================================================================================================
+//
+// GameModeMenuScreen class
+//
+//=================================================================================================================================
+
 GameModeMenuScreen::GameModeMenuScreen(ScreenManager* screenManager)
 	: MenuScreen(screenManager)
 {
@@ -18,19 +34,83 @@ void GameModeMenuScreen::init()
 {
 	MenuScreen::init();
 
+	m_btnContainerDest.Size = MyVec2(m_textures[TEXTURE_COMMON_HPANEL].getWidth(), m_textures[TEXTURE_COMMON_HPANEL].getHeight());
+
 	// Buttons
 	m_imageBtns[IMAGE_BTN_PLAY_ONLINE].init("btn_play_online", MyVec2(), m_textures[TEXTURE_GAME_MODE_BTN_ONLINE]);
-	m_imageBtns[IMAGE_BTN_PLAY_SOLO].init("btn_play_solo", MyVec2(200, 0), m_textures[TEXTURE_GAME_MODE_BTN_SOLO]);
+	m_imageBtns[IMAGE_BTN_PLAY_SOLO].init("btn_play_solo", MyVec2(), m_textures[TEXTURE_GAME_MODE_BTN_SOLO]);
 
 	for (size_t i = 0; i < NUM_IMAGE_BTNS; i++)
 	{
 		m_imageBtns[i].addPressListener(this);
+	}
+
+	{
+		MyColor textColor = MyColor(212.0f / 255.0f, 170.0f / 225.0f, 0.0f / 255.0f);
+
+		m_textBtns[TEXT_BTN_BACK].init(
+			"btn_back", 
+			MyVec2(), 
+			m_textures[TEXTURE_COMMON_BTN_BACKGROUND], 
+			"BACK", 
+			m_fonts[FONT_BAUHAUS93_26], 
+			textColor);
+	}
+
+	for (int i = 0; i < NUM_TEXT_BTNS; i++)
+	{
+		m_textBtns[i].addPressListener(this);
+	}
+	
+	// Labels
+	{
+		MyColor textColor = MyColor(212.0f / 255.0f, 170.0f / 225.0f, 0.0f / 255.0f);
+		m_labels[LABEL_SCREEN_TITLE].init("", MyVec2(), m_fonts[FONT_BAUHAUS93_26], "GAME MODE", textColor);
 	}
 }
 
 void GameModeMenuScreen::resize(int width, int height)
 {
 	MenuScreen::resize(width, height);
+
+	// Button container
+	m_btnContainerDest.Pos = MyVec2(
+		0.5f * (m_width - m_btnContainerDest.Size.x),
+		0.5f * (m_height - m_btnContainerDest.Size.y));
+
+	// Choose game mode button
+	{
+		MyVec2 btnsSize = MyVec2(
+			m_imageBtns[IMAGE_BTN_PLAY_SOLO].getSize().x + m_imageBtns[IMAGE_BTN_PLAY_ONLINE].getSize().x + BTN_HDIST,
+			m_imageBtns[IMAGE_BTN_PLAY_SOLO].getSize().y);
+		MyVec2 pos = m_btnContainerDest.Pos + 0.5f * (m_btnContainerDest.Size - btnsSize);
+
+		m_imageBtns[IMAGE_BTN_PLAY_SOLO].setPos(pos);
+
+		pos.x += m_imageBtns[IMAGE_BTN_PLAY_SOLO].getSize().x + BTN_HDIST;
+
+		m_imageBtns[IMAGE_BTN_PLAY_ONLINE].setPos(pos);
+	}
+
+	// Back button
+	{
+		MyVec2 pos = m_btnContainerDest.Pos + 
+			MyVec2(
+			0.5f * (m_btnContainerDest.Size.x - m_textBtns[TEXT_BTN_BACK].getSize().x),
+			m_btnContainerDest.Size.y + BTN_BACK_MARGIN);
+
+		m_textBtns[TEXT_BTN_BACK].setPos(pos);
+	}
+
+	// Title
+	{
+		MyVec2 pos = m_btnContainerDest.Pos +
+			MyVec2(
+			0.5f * (m_btnContainerDest.Size.x - m_labels[LABEL_SCREEN_TITLE].getSize().x),
+			-(SCREEN_TITLE_MARGIN + m_labels[LABEL_SCREEN_TITLE].getSize().y));
+
+		m_labels[LABEL_SCREEN_TITLE].setPos(pos);
+	}
 }
 
 void GameModeMenuScreen::update(void* utilObjs)
@@ -44,6 +124,16 @@ void GameModeMenuScreen::update(void* utilObjs)
 	{
 		m_imageBtns[i].update(*globalUtilObjs->userInput);
 	}
+	for (int i = 0; i < NUM_TEXT_BTNS; i++)
+	{
+		m_textBtns[i].update(*globalUtilObjs->userInput);
+	}
+
+	// Labels
+	for (int i = 0; i < NUM_LABELS; i++)
+	{
+		m_labels[i].update(*globalUtilObjs->userInput);
+	}
 }
 
 void GameModeMenuScreen::render(void* utilObjs)
@@ -52,10 +142,22 @@ void GameModeMenuScreen::render(void* utilObjs)
 
 	GLOBAL_UTIL_OBJS* globalUtilObjs = (GLOBAL_UTIL_OBJS*)utilObjs;
 
+	globalUtilObjs->spriteBatch->renderTexture2D(&m_textures[TEXTURE_COMMON_HPANEL], m_btnContainerDest);
+
 	// Buttons
 	for (int i = 0; i < NUM_IMAGE_BTNS; i++)
 	{
 		m_imageBtns[i].render(*globalUtilObjs->spriteBatch);
+	}
+	for (int i = 0; i < NUM_TEXT_BTNS; i++)
+	{
+		m_textBtns[i].render(*globalUtilObjs->spriteBatch);
+	}
+
+	// Labels
+	for (int i = 0; i < NUM_LABELS; i++)
+	{
+		m_labels[i].render(*globalUtilObjs->spriteBatch);
 	}
 }
 
@@ -63,10 +165,13 @@ void GameModeMenuScreen::OnPress(const IOnPressListener::Data& data)
 {
 	if (data.Id == "btn_play_online")
 	{
-
 	}
 	else if (data.Id == "btn_play_solo")
 	{
 		m_screenManager->activeScreen("PlayScreen");
+	}
+	else if (data.Id == "btn_back")
+	{
+		m_screenManager->activeScreen("MainMenuScreen");
 	}
 }

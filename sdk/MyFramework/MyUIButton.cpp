7105244d252a5ade69f8489e3881f0992107255b
hoainam1593 <hoainam1593@yahoo.com>
2015-06-06 17:57:41 +0700

    nam

--- sdk/MyFramework/MyUIButton.cpp
+++ sdk/MyFramework/MyUIButton.cpp
@@ -3,12 +3,21 @@
 
 //===========================================================================================================
 //
+// Constants
+//
+//===========================================================================================================
+
+static const float SCALE_WHEN_PRESSED = 0.95f;
+
+//===========================================================================================================
+//
 // UIImageButton class
 //
 //===========================================================================================================
 
 UIImageButton::UIImageButton()
-	: m_background(nullptr)
+	: m_background(nullptr),
+	m_scale(1.0f)
 {
 }
 
@@ -22,6 +31,21 @@ void UIImageButton::init(const MyString& id, const MyVec2& pos, Texture& backgro
 	UIWidget::init(id, pos, MyVec2(m_background->getWidth(), m_background->getHeight()));
 }
 
+void UIImageButton::update(UserInput& userInput)
+{
+	m_scale = 1.0f;
+	if (m_status == ACTIVE)
+	{
+		MyVec2 pos;
+		if (userInput.pointer_Holding(pos) && isInside(pos, m_bounding))
+		{
+			m_scale = SCALE_WHEN_PRESSED;
+		}
+	}
+
+	UIWidget::update(userInput);
+}
+
 void UIImageButton::render(SpriteBatch& spriteBatch, const Rect2D* viewport)
 {
 	if (m_status == HIDDEN)
@@ -29,7 +53,9 @@ void UIImageButton::render(SpriteBatch& spriteBatch, const Rect2D* viewport)
 		return;
 	}
 
+	m_bounding.Size *= m_scale;
 	spriteBatch.renderTexture2D(m_background, m_bounding, nullptr, 0, viewport);
+	m_bounding.Size *= 1.0f / m_scale;
 }
 
 //===========================================================================================================
@@ -71,6 +97,8 @@ void UITextButton::render(SpriteBatch& spriteBatch, const Rect2D* viewport)
 	{
 		MyVec2 pos = getPos() + 0.5f * (getSize() - m_label.getSize());
 		m_label.setPos(pos);
+		m_label.accSize(m_scale);
 		m_label.render(spriteBatch, viewport);
+		m_label.accSize(1.0f / m_scale);
 	}
 }
